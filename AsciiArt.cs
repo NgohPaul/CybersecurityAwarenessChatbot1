5532db137ef771152a7ee6087db9952ef4839cce
lab_services_student <lab_services_student@EMERISLABVMBCAD>
2026-03-27 01:46:27 +0200

    Add project files.

---
+++ AsciiArt.cs
@@ -0,0 +1,23 @@
+﻿namespace CybersecurityAwarenessChatbot
+{
+    public class AsciiArt
+    {
+        public static void DisplayLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine("=======================================");
+            Console.WriteLine("   CYBERSECURITY AWARENESS CHATBOT");
+            Console.WriteLine("=======================================");
+            Console.WriteLine(@" 
+        [###########]
+        [  SECURE   ]
+        [   YOUR    ]
+        [   DATA    ]
+        [###########]
+            ");
+
+            Console.ResetColor();
+        }
+    }
+}
---
+++ AudioPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Media;
+
+namespace CybersecurityAwarenessChatbot
+{
+    public class AudioPlayer
+    {
+        public static void PlayGreeting()
+        {
+            try
+            {
+                SoundPlayer player = new SoundPlayer("file_example_WAV_1MG.wav");
+                player.PlaySync();
+            }
+            catch
+            {
+                Console.WriteLine("Audio file not found.");
+            }
+        }
+    }
+}
